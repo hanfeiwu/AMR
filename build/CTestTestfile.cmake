85dd915871751978d7e512cec56ae8c57dd70166
Your Name <you@example.com>
2021-11-02 19:01:14 +0800

    amr_control

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/whf/AMR/src
+# Build directory: /home/whf/AMR/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("control")
